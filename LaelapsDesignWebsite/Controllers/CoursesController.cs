04337a9c508969662074766e72b8f5472287e4eb
airborne <qweasdz76@live.com>
2019-04-22 19:13:33 +0800

    course图片修正

--- LaelapsDesignWebsite/Controllers/CoursesController.cs
+++ LaelapsDesignWebsite/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LaelapsDesignWebsite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,17 +14,30 @@ namespace LaelapsDesignWebsite.Controllers
         // GET: /<controller>/
         public ActionResult Index()
         {
-            Dictionary<string, string> Courses = new Dictionary<string, string>()
+            //Dictionary<string, string> Courses = new Dictionary<string, string>()
+            //{
+            //    {"ENVIRONMENT DESIGN","/wwwroot/images/Courses/1.png" },
+            //    {"ADVANCED ENVIRONMENT DESIGN","/wwwroot/images/Courses/2.png" },
+            //    {"CHARACTER DESIGN I","/wwwroot/images/Courses/3.png" },
+            //    {"CHARACTER DESIGN II","/wwwroot/images/Courses/4.jpg" },
+            //    {"FUNDAMENTALS OF CHARACTER DESIGN","/wwwroot/images/Courses/5.jpg" },
+            //    {"WEAPONS & PROPS FOR GAMES","/wwwroot/images/Courses/6.png" },
+            //    {"ENTERTAINMENT DEISGN","/wwwroot/images/Courses/7.png" },
+            //    {"ADVANCED MENTORSHIP","/wwwroot/images/Courses/8.png" }
+            //};
+
+            List<Course> Courses = new List<Course>()
             {
-                {"ENVIRONMENT DESIGN","/wwwroot/images/Courses/1.png" },
-                {"ADVANCED ENVIRONMENT DESIGN","/wwwroot/images/Courses/2.png" },
-                {"CHARACTER DESIGN I","/wwwroot/images/Courses/3.png" },
-                {"CHARACTER DESIGN II","/wwwroot/images/Courses/4.jpg" },
-                {"FUNDAMENTALS OF CHARACTER DESIGN","/wwwroot/images/Courses/5.jpg" },
-                {"WEAPONS & PROPS FOR GAMES","/wwwroot/images/Courses/6.png" },
-                {"ENTERTAINMENT DEISGN","/wwwroot/images/Courses/7.png" },
-                {"ADVANCED MENTORSHIP","/wwwroot/images/Courses/8.png" }
+                new Course(){Title="ENV", Text="ENVIRONMENT DESIGN",Img="/wwwroot/images/Courses/1.png"},
+                new Course(){Title="AED", Text="ADVANCED ENVIRONMENT DESIGN",Img="/wwwroot/images/Courses/2.png"},
+                new Course(){Title="CD1", Text="CHARACTER DESIGN I",Img="/wwwroot/images/Courses/3.png"},
+                new Course(){Title="CD2", Text="CHARACTER DESIGN II",Img="/wwwroot/images/Courses/4.jpg"},
+                new Course(){Title="FCD", Text="FUNDAMENTALS OF CHARACTER DESIGN",Img="/wwwroot/images/Courses/5.jpg"},
+                new Course(){Title="WPG", Text="WEAPONS & PROPS FOR GAMES",Img="/wwwroot/images/Courses/6.png"},
+                new Course(){Title="ENT", Text="ENTERTAINMENT DEISGN",Img="/wwwroot/images/Courses/7.png"},
+                new Course(){Title="ADM", Text="ADVANCED MENTORSHIP",Img="/wwwroot/images/Courses/8.png"}
             };
+
             ViewData["list"] = Courses;
             return View();
         }
---
+++ LaelapsDesignWebsite/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaelapsDesignWebsite.Models
+{
+    public class Course
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Img { get; set; }
+    }
+}
